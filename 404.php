020a679a567cff47e53a0e2d1bcac2e5cd42b9db
Marcel van der Hoek <marcel@lekkerboeiend.com>
2015-10-05 15:23:49 +0200

    Removed files, modified 404
    - Removed unused files, functions and references to functions.
    - Modified the 404 template.

--- 404.php
+++ 404.php
@@ -9,50 +9,18 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<article id="404" class="bericht">
+    
+    <header class="bericht-header">
+		<h1 class="page-title">Oeps! De pagina die je zoekt bestaat niet (meer).</h1>
+	</header><!-- .bericht-header -->
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ann-adores' ); ?></h1>
-				</header><!-- .page-header -->
+	<div class="entry-content">
+		<p>Op het adres dat je hebt ingetikt of aangeklikt is niets (meer) te vinden. Hieronder staat een lijstje met mijn laatste berichten. Hopelijk vind je daar iets leuks tussen!</p>
+		<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ann-adores' ); ?></p>
+	</div><!-- .entry-content -->
 
-					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( ann_adores_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ann-adores' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ann-adores' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</article>
 
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -62,7 +62,8 @@ function ann_adores_setup() {
 	/*
 	 * Enable support for Post Formats.
 	 * See https://developer.wordpress.org/themes/functionality/post-formats/
-	 */
+	 * TO DO!
+
 	add_theme_support( 'post-formats', array(
 		'aside',
 		'image',
@@ -71,11 +72,7 @@ function ann_adores_setup() {
 		'link',
 	) );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'ann_adores_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+	 */
 }
 endif; // ann_adores_setup
 add_action( 'after_setup_theme', 'ann_adores_setup' );
@@ -88,7 +85,7 @@ add_action( 'after_setup_theme', 'ann_adores_setup' );
  * @global int $content_width
  */
 function ann_adores_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'ann_adores_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters( 'ann_adores_content_width', 872 );
 }
 add_action( 'after_setup_theme', 'ann_adores_content_width', 0 );
 
@@ -116,10 +113,6 @@ add_action( 'widgets_init', 'ann_adores_widgets_init' );
 function ann_adores_scripts() {
 	wp_enqueue_style( 'ann-adores-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'ann-adores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'ann-adores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -127,29 +120,14 @@ function ann_adores_scripts() {
 add_action( 'wp_enqueue_scripts', 'ann_adores_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
 
 /**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
 
 add_filter( 'the_content_more_link', 'modify_read_more_link' );
 function modify_read_more_link() {
